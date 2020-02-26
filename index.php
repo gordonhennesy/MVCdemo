9a73a8ebe4fd6a43715626106a0594d89dd1b3f2
Gordon Hennesy <gordon@zebrafish.org>
2020-02-26 12:55:40 -0800

    Removing a thing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?PHP
 
 	echo "MVC Demo Gordon Hennesy 2-26-2020<br>";
-	$connection_string = "host=localhost port=5432 dbname=demo user=postgres password=OCCU2020";
+	$connection_string = "host=localhost port=5432 dbname=demo user=postgres password=********";
 	$conn = pg_connect ( $connection_string );
 	$result = pg_query($conn, "select * from name");
 	while( $result_arr = pg_fetch_array($result)) {
